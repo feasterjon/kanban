49fd2f18f455f219f33897c4e9558503f1d4efab
Feaster <feaster@gmail.com>
2022-04-06 12:55:40 -0500

    main.js, kanban index.js refactor

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,7 @@
 /*
 Title: Kanban
 Author: Jonathan Feaster, JonFeaster.com
-Date: 2022-04-04
+Date: 2022-04-06
 */
 
 import CONFIG from './config.js';
@@ -10,12 +10,14 @@ import Kanban from "./modules/kanban/index.js";
 import { LocalDataAdmin as LocalData } from './modules/localdata/admin.js';
 
 class Main {
-  constructor() {
-    this.data = CONFIG.data[0].attributes;
+  constructor(data) {
+    this.data = data;
     this.effects = new jmodules.Effects();
     this.events = new jmodules.Events();
     this.interaction = new jmodules.Interaction();
     this.localData = new LocalData(this.data.localData);
+  }
+  render() {
     if (this.data) {
       const kanbanData = this.localData.readField("data");
       if (!kanbanData || kanbanData[0].type !== this.data.dataName) {
@@ -53,10 +55,11 @@ class Main {
           location.href = './';
         }
       }
-      new Kanban(
+      const kanban = new Kanban(
         this.data,
         document.querySelector(".kanban")
       );
+      kanban.render();
       this.localData.updateView(); // update view
       this.events.blur(this.data.headingId, () => {
         let output = {};
@@ -109,4 +112,5 @@ class Main {
   }
 }
 
-const main = new Main();
+const main = new Main(CONFIG.data[0].attributes);
+main.render();
--- src/assets/js/modules/kanban/index.js
+++ src/assets/js/modules/kanban/index.js
@@ -1,7 +1,7 @@
 /*
 Title: Kanban
 Author: Jonathan Feaster, JonFeaster.com
-Date: 2022-04-04
+Date: 2022-04-06
 */
 
 import Column from './view/column.js';
@@ -13,11 +13,12 @@ export default class Kanban {
     this.swimlanes = data.swimlanes;
 		this.root = root;
     this.localData = new LocalData(this.data.localData);
-    
+	}
+  render() {
     let columns = Object.values(this.swimlanes);
     for (let column of columns) {
       const columnView = new Column(column.id, column.title);
       this.root.appendChild(columnView.elements.root);
     }
-	}
+  }
 }
